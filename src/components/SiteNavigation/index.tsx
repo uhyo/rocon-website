d41739047c74f05e3df3e1ffc5205b82ccc2a353
uhyo <uhyo@uhy.ooo>
2020-08-10 18:04:05 +0900

    GitHub link

--- src/components/SiteNavigation/index.tsx
+++ src/components/SiteNavigation/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "rocon/react";
 import { toplevelRoutes } from "~/routes";
+import { ExternalLink } from "~/util/ExternalLink";
 import { siteNavigationCss } from "./styles";
 
 export const SiteNavigation: React.FC = () => {
@@ -9,6 +10,11 @@ export const SiteNavigation: React.FC = () => {
       <ul>
         <li>
           <Link route={toplevelRoutes._.tutorial}>Rocon Tutorial</Link>
+        </li>
+        <li>
+          <ExternalLink href="https://github.com/uhyo/rocon">
+            GitHub
+          </ExternalLink>
         </li>
       </ul>
     </nav>
--- src/components/SiteNavigation/styles.ts
+++ src/components/SiteNavigation/styles.ts
@@ -6,8 +6,16 @@ export const siteNavigationCss = css`
   border-bottom: 1px solid var(--nc-bg-3);
 
   & > ul {
+    display: flex;
+    flex-flow: row wrap;
     margin: 0;
     padding: 0;
     list-style-type: none;
+
+    & > li {
+      box-sizing: border-fox;
+      flex: 20% 0 0;
+      padding-right: 1em;
+    }
   }
 `;
